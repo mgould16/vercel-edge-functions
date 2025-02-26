72cae8d95af5846cf6d1a135fcf94f5bab5ad88f
Mark Gould <m.gould16@gail.com>
2025-02-26 14:42:20 +0000

    added geo-test page

---
+++ app/geo-test/page.tsx
@@ -0,0 +1,15 @@
+export default function GeoTestPage() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🌍 Edge Function Geo-Redirect Test</h1>
+      <p>
+        If the Edge Function is working, you should be redirected based on your
+        location.
+      </p>
+      <p>
+        If not, you're seeing this page instead, which means the Edge Function
+        is not running.
+      </p>
+    </div>
+  );
+}
