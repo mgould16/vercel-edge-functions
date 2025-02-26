da036504aacb2dc50fc4ee1439fd898046839092
Mark Gould <m.gould16@gail.com>
2025-02-26 14:49:33 +0000

    removed all &nsp

--- app/geo-test/page.tsx
+++ app/geo-test/page.tsx
@@ -3,7 +3,7 @@ export default function GeoTestPage() {
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>Edge Function Geo-Redirect Test</h1>
       <p>If the Edge Function is working, you should be redirected based on your location.</p>
-      <p>If not, you&apos;re seeing this page instead, which means the Edge Function is not running.</p>
+      <p>If not, you are seeing this page instead, which means the Edge Function is not running.</p>
     </div>
   );
 }
